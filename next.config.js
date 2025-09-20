18fe9f950be302dc7ff787d94830f5f7408accfa
RodolfoT93 <rodolfo_turini1993@icloud.com>
2025-09-20 16:51:33 -0300

    Atualizar o next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/meu-app-prescricao',
   images: {
-    unoptimized: true,
-  },
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
